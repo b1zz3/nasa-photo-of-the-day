57386e3dd6918f9ea24605dd558a7dbf075c9231
b1zz3 <donhager23@gmail.com>
2019-11-22 00:52:43 -0500

    Add react card styles and sizing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
 import Fetch from "./components/fetch";
-import { Button } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
       <div className="main-container">
-        <Button color="info">Reactstrap Button</Button>
+        <h1>Nasa - Astronomy Picture of the Day</h1>
         <Fetch />
       </div>
     </div>
