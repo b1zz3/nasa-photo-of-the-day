88d5379ccf07bec63f8131021889e07dc2f0f630
b1zz3 <donhager23@gmail.com>
2019-11-21 19:51:52 -0500

    Install reactstrap and bootstrap, test button from lib --success

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Fetch from "./components/fetch";
+import { Button } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
       <div className="main-container">
+        <Button>Reacstrap Button</Button>
         <Fetch />
       </div>
     </div>
